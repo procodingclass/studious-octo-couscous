3bfa58e82979b1594dbad8575573496c94e00baa
vishalgddam873 <vishal18@navgurukul.org>
2022-06-16 17:43:30 +0530

    Initial comment

--- game.py
+++ game.py
@@ -38,9 +38,7 @@ background_surf = loadImages(["assets/environment.png"])
 ground_surf = loadImages(["assets/ground.png"])
 door_surf = loadImages(["assets/doorOpen.png"])
 win_surf = loadImages(["assets/win.png"])
-game_over_surf = loadImages(["assets/gameOver.png"])
 
-platform_surf = loadImages(["assets/platform.png"])
 
 #player_surf = loadImages(["assets/joe1.png"])
 cloud_surf = loadImages(["assets/cloud.png"])
@@ -55,21 +53,14 @@ background_surf = scaleImages(background_surf, screen_width, 700)
 color_surf = scaleImages(color_surf, screen_width, 400)
 door_surf= scaleImages(door_surf, 100,100)
 cloud_surf = scaleImages(cloud_surf, 100, 50)
-game_over_surf = scaleImages(game_over_surf, 230, 150)
 #player_surf = scaleImages(player_surf, 40, 63)
 ground_surf = scaleImages(ground_surf, screen_width, 90)
-platform_surf = scaleImages(platform_surf, 100, 40)
-
 
 
 # sprites
 #player_rect = pygame.Rect(620,screen_height-145,40,63)
 ground_rect = pygame.Rect(0,screen_height- 88,screen_width,40)
 door_rect = pygame.Rect(screen_width-130, screen_height-190, 100,100)
-platform1_rect = pygame.Rect(220, screen_height - 220, 100,40)
-platform2_rect = pygame.Rect(570, screen_height - 220, 100,40)
-
-
 
 
 
@@ -84,14 +75,13 @@ def draw_arena():
     global ground_surf, door_surf, win_surf
     global cloud_surf, ground_rect, door_rect
     global cloud1X, cloud2X, cloud3X
-    global screen, platform_surf,platform1_rect, platform2_rect
+    global screen
 
     screen.blit(color_surf, (0, 0))
     screen.blit(background_surf, (0, 0))
     screen.blit(ground_surf, (0, screen_height-90))
     screen.blit(door_surf,door_rect)
-    # screen.blit(platform_surf,platform1_rect)
-    # screen.blit(platform_surf,platform2_rect)
+
 
 
     screen.blit(cloud_surf, (cloud1X, 30))
@@ -169,7 +159,7 @@ def check_collision(static_rect, moving_rect, vel_x, vel_y):
 
 def gameplay():
     global door_surf, win_surf
-    global ground_rect, screen, platform1_rect, platform2_rect
+    global ground_rect, screen
 
     draw_arena()
     try:
@@ -178,18 +168,7 @@ def gameplay():
         if(main.player_rect != None and main.player_surf != None):
             if(main.player_rect.colliderect(door_rect)):
                 screen.blit(win_surf[0],(screen_width / 2 - 140,100))
-                img_width = int(main.player_surf[0].get_width() * 0.99)
-                img_height = int(main.player_surf[0].get_height() * 0.99)
-                main.player_surf = scaleImages(main.player_surf, img_width,img_height)
 
-            if(main.gamestates == "over"):
-                screen.blit(game_over_surf,(screen_width / 2 - 120,100))
-
-
-            # checking ground collision
-            main.player_vel_x, main.player_vel_y = check_collision(ground_rect, main.player_rect, main.player_vel_x, main.player_vel_y)
-            main.player_vel_x, main.player_vel_y = check_collision(platform1_rect, main.player_rect, main.player_vel_x, main.player_vel_y)
-            main.player_vel_x, main.player_vel_y = check_collision(platform2_rect, main.player_rect, main.player_vel_x, main.player_vel_y)
 
 
     except:
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ player_surf = pygame.image.load("assets/joe/0.png").convert_alpha()
 player_surf = pygame.transform.scale(player_surf, (30, 43))
 
 # Player Rectangle
-player_rect = pygame.Rect(110, screen_height - 130 ,30,43) # x, y, width, height
+player_rect = pygame.Rect(610, screen_height - 130 ,30,43) # x, y, width, height
 
 
 while True:
